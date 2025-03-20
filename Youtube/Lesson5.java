bd64a52f498985e1cba7654fe9c115e566a754a5
mdanassaif <oliwertwist9@gmail.com>
2025-03-20 15:23:11 +0530

    add lesson 5

---
+++ Youtube/Lesson5.java
@@ -0,0 +1,39 @@
+/// Classes & Objects /////
+
+/*
+
+1. Process vs Object Oriented
+
+2. Instance Variables and Methods
+
+3. Declaring and Using Objects
+
+4. Class vd Object
+
+5. This and Static Keyword
+
+6. Constructors & Code Blocks
+
+7. Stack vs Heap Memory
+
+8. Primitive vs Refrence Types
+
+
+9. Variables Scopes
+
+10. Garbage Collection & FInalize
+
+
+
+
+
+
+*/
+
+
+
+public class Lesson5{
+	public static void main(String[] args){
+		System.out.println("Yes");
+	}
+}
